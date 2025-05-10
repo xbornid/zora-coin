40327416d33f18f17f08487cbf67feddbd15b14a
xbornid <36538144+xbornid@users.noreply.github.com>
2025-05-11 05:49:35 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './context/**/*.{js,jsx}'
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './context/**/*.{js,jsx}'],
+  theme: { extend: {} },
   plugins: [],
 };
